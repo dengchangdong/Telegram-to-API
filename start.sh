c4d6e87279e64928342c3206f4a08c79a5d8f272
Deng Changdong <dengchangdong@gmail.com>
2024-09-22 08:28:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,5 @@
 # Set the default port if not defined
 PORT=${PORT:-8000}
 
-# Force to run with only one worker (single thread)
-WORKERS=1
-
-# Run the uvicorn server without uvloop for compatibility
-uvicorn app.main:app --workers "$WORKERS" --host=0.0.0.0 --port="$PORT" --http h11
+# Force to run uvicorn with one worker (single thread)
+uvicorn app.main:app --workers 1 --host=0.0.0.0 --port="$PORT" --http h11
