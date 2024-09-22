4510e68c4206e7cfc3f4e1a50e398e843a128dd3
Deng Changdong <dengchangdong@gmail.com>
2024-09-22 09:19:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Set the default port if not defined
-PORT=${PORT:-8080}
-
-WORKERS=6
+PORT=${PORT:-8000}
 
 # Force uvicorn to use default asyncio event loop
-UVICORN_CMD="uvicorn app.main:app --host=0.0.0.0  --workers "$WORKERS" --port=$PORT --loop asyncio"
+UVICORN_CMD="uvicorn app.main:app --host=0.0.0.0 --port=$PORT --loop asyncio"
 
 # Run the uvicorn server with default asyncio event loop
 $UVICORN_CMD
