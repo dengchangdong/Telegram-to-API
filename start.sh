ea5c136a878f66dd1a42f9bb9149872eb6c10a00
Deng Changdong <dengchangdong@gmail.com>
2024-09-22 08:21:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ PORT=${PORT:-8000}
 # Force to run with only one worker (single thread)
 WORKERS=1
 
-# Run the uvicorn server with the specified settings
-uvicorn app.main:app --workers "$WORKERS" --host=0.0.0.0 --port="$PORT" --loop uvloop --http h11
+# Run the uvicorn server without uvloop for compatibility
+uvicorn app.main:app --workers "$WORKERS" --host=0.0.0.0 --port="$PORT" --http h11
