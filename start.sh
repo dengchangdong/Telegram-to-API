78d3a86ec6606054edbb926775412b532c8fac8c
Deng Changdong <dengchangdong@gmail.com>
2024-09-22 08:35:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@
 PORT=${PORT:-8000}
 
 # Force to run uvicorn with one worker (single thread)
-uvicorn app.main:app --workers 1 --host=0.0.0.0 --port="$PORT" --http h11
+uvicorn app.main:app --host=0.0.0.0 --port="$PORT"
+
