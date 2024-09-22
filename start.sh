7d5689ee3ceeaa8c19c9b553ecfd97ed9c7ab8db
Deng Changdong <dengchangdong@gmail.com>
2024-09-22 10:10:42 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 # Set the default port if not defined
 PORT=${PORT:-8000}
 
-# Force uvicorn to use default asyncio event loop
-UVICORN_CMD="uvicorn app.main:app --host=0.0.0.0 --port=$PORT --loop asyncio"
+# Run the Hypercorn server
+HYPERCORN_CMD="hypercorn app.main:app --bind 0.0.0.0:$PORT --worker-class asyncio"
 
-# Run the uvicorn server with default asyncio event loop
-$UVICORN_CMD
+# Execute the command
+$HYPERCORN_CMD
